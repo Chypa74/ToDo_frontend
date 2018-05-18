3f1036e45df05cc898feb992cfe96d481734d67a
Dmitry Shuvalov <mitiashuv74@mail.ru>
2018-05-18 18:28:09 +0300

    Some changes at the MainSection component. Also change logic for adding new todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import * as types from '../constants/ActionTypes';
 
-export const addTodo = text => ({ type: types.ADD_TODO, text });
+export const addTodo = title => ({ type: types.ADD_TODO, title });
 
 export const deleteTodo = id => ({ type: types.DELETE_TODO, id });
 
--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../Footer';
 import { connect } from 'react-redux';
+import TodoTextInput from '../TodoTextInput';
 import * as TodoActions from '../../actions';
 import { bindActionCreators } from 'redux';
 import { getCompletedTodoCount } from '../../selectors';
 import TodoList from '../TodoList';
 
 const MainSection = ({ todosCount, completedCount, actions }) => (
-  <section className="main">
-    {todosCount && (
-      <span>
-        <input
-          className="toggle-all"
-          type="checkbox"
-          checked={completedCount === todosCount}
-        />
-        <label onClick={actions.completeAllTodos} />
-      </span>
-    )}
-    <TodoList />
-    {todosCount && (
-      <Footer
-        completedCount={completedCount}
-        activeCount={todosCount - completedCount}
-        onClearCompleted={actions.clearCompleted}
+  <Fragment>
+    <header className="header">
+      <TodoTextInput
+        newTodo
+        onSave={title => {
+          if (title.length !== 0) {
+            actions.addTodo(title);
+          }
+        }}
+        placeholder="What needs to be done?"
       />
-    )}
-  </section>
+    </header>
+    <section className="main">
+      {todosCount ? (
+        <span>
+          <input
+            className="toggle-all"
+            type="checkbox"
+            checked={completedCount === todosCount}
+          />
+          <label onClick={actions.completeAllTodos} />
+        </span>
+      ) : null}
+      <TodoList />
+      {todosCount ? (
+        <Footer
+          completedCount={completedCount}
+          activeCount={todosCount - completedCount}
+          onClearCompleted={actions.clearCompleted}
+        />
+      ) : null}
+    </section>
+  </Fragment>
 );
 
 MainSection.propTypes = {
