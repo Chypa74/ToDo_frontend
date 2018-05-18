557e93ce9c51339c2aae2828c8ae4947b984cd5f
Dmitry Shuvalov <mitiashuv74@mail.ru>
2018-05-18 14:57:34 +0300

    Create TextArea component

---
+++ src/components/Textarea/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import TextareaAutoresize from 'react-textarea-autosize';
+import './Textarea.css';
+
+const Textarea = ({ inputRef, className, ...props }) => (
+  <TextareaAutoresize
+    inputRef={inputRef}
+    className={classNames('Textarea', className)}
+    {...props}
+  />
+);
+
+Textarea.propTypes = {
+  // TODO:
+};
+
+export default Textarea;
